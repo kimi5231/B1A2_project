3b1e18e2620012fc3576d748452075fc18f5cb2b
Lim Sooyoung <djwjod@naver.com>
2025-06-15 23:10:16 +0900

    B1A2_project1 작업 디렉터리를 출력 디렉터리와 동일하게 수정 후 그에 맞게 Resources 경로 수정

--- B1A2_project2/B1A2_project1/Game.cpp
+++ B1A2_project2/B1A2_project1/Game.cpp
@@ -25,7 +25,7 @@ void Game::Init(HWND hwnd)
 
 	// 상대 경로 path 알아내는 코드
 	_resourcePath = std::filesystem::current_path().relative_path();
-	std::filesystem::path _resourcePath = std::filesystem::current_path().parent_path().parent_path() / "B1A2_project2\\Resources";
+	std::filesystem::path _resourcePath = std::filesystem::current_path().parent_path().parent_path().parent_path() / "Resources";
 
 	// 폰트 경로
 	std::filesystem::path fullPath = _resourcePath / L"Font\\DungGeunMo.ttf";
