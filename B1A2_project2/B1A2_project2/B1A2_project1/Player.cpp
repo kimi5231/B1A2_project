b1996103171062401192ecb2d6a7e00e775261d3
Im caheeun <dev.chaeng@gmail.com>
2025-07-16 19:30:17 +0900

    Player 클래스 Input 관련 코드 기존 코드로 수정

--- B1A2_project2/B1A2_project2/B1A2_project1/Player.cpp
+++ B1A2_project2/B1A2_project2/B1A2_project1/Player.cpp
@@ -203,169 +203,40 @@ void Player::Render(HDC hdc)
 
 void Player::TickIdle()
 {
-	/*if (GetDialogue()->GetState() == DialogueState::Running || GetDialogue()->GetState() == DialogueState::Wait)
-		return;*/
-
-	float deltaTime = GET_SINGLE(TimeManager)->GetDeltaTime();
-	static float sumTime = 0.0f;
-
-	sumTime += deltaTime;
-
-	_keyPressed = true;
-
-	if (GET_SINGLE(InputManager)->GetButton(KeyType::A))
-	{
-		SetDir(DIR_LEFT);
-		SetState(MOVE);
-	}
-	else if (GET_SINGLE(InputManager)->GetButton(KeyType::D))
-	{
-		SetDir(DIR_RIGHT);
-		SetState(MOVE);
-	}
-	else if (GET_SINGLE(InputManager)->GetButtonDown(KeyType::SpaceBar))
-	{
-		// 이미 점프 중이면 리턴
-		if (!_isGround && _isAir)
-			return;
-
-		_isGround = false;
-		_isAir = true;
-
-		SetState(JUMP);
-
-		_ySpeed = -_playerStat->jumpSpeed;
-	}
-	else if (GET_SINGLE(InputManager)->GetButton(KeyType::S))
-	{
-		SetState(DUCK_DOWN);
-	}
-	else if (GET_SINGLE(InputManager)->GetButtonDown(KeyType::LeftMouse))
-	{
-		if (_isCloseAtk)
-			SetState(CLOSE_ATTACK);
-		else
-			SetState(LONG_ATTACK);
-	}
-	else if (GET_SINGLE(InputManager)->GetButtonDown(KeyType::RightMouse))
-	{
-		if (GetSkillPoint() >= 3)
-		{
-			SubtractSkillPoint(3);
-
-			_skillTimer = 0.f;
-
-			_leftInputCount = 0;
-			_rightInputCount = 0;
-
-			SetState(SKILL_READY);
-		}
-	}
-	else
-	{
-		_keyPressed = false;
-
-		if (_info.state() == IDLE)
-			UpdateAnimation();
-	}
+	
 }
 void Player::TickMove()
 {
-	/*if (GetDialogue()->GetState() == DialogueState::Running || GetDialogue()->GetState() == DialogueState::Wait)
-		return;*/
-
 	float deltaTime = GET_SINGLE(TimeManager)->GetDeltaTime();
-	static float sumTime = 0.0f;
 
-	sumTime += deltaTime;
-
-	if (GET_SINGLE(InputManager)->GetButton(KeyType::A))
+	switch (_info.dir())
 	{
-		SetDir(DIR_LEFT);
+	case DIR_LEFT:
 		_pos.x -= _playerStat->runSpeed * deltaTime;
-	}
-	else if (GET_SINGLE(InputManager)->GetButton(KeyType::D))
-	{
-		SetDir(DIR_RIGHT);
+		break;
+	case DIR_RIGHT:
 		_pos.x += _playerStat->runSpeed * deltaTime;
-	}
-	else
-	{
-		SetState(IDLE); // 이동 키를 뗐을 때 Idle 상태로 변경
-	}
-
-	if (GET_SINGLE(InputManager)->GetButtonDown(KeyType::SpaceBar))
-	{
-		// 이미 점프 중이면 리턴
-		if (!_isGround && _isAir)
-			return;
-
-		_isGround = false;
-		_isAir = true;
-
-		SetState(JUMP);
-
-		_ySpeed = -_playerStat->jumpSpeed;
-	}
-	else if (GET_SINGLE(InputManager)->GetButton(KeyType::S))
-	{
-		SetState(DUCK_DOWN_MOVE);
-	}
-	else if (GET_SINGLE(InputManager)->GetButtonDown(KeyType::LeftMouse))	// Normal Attack
-	{
-		if (_isCloseAtk)
-			SetState(CLOSE_ATTACK);
-		else
-			SetState(LONG_ATTACK);
-
-	}
-	else if (GET_SINGLE(InputManager)->GetButtonDown(KeyType::RightMouse))	// Skill
-	{
-		SetState(SKILL_READY);
+		break;
 	}
 }
 
 void Player::TickDuckDown()
 {
-	if (GET_SINGLE(InputManager)->GetButtonDown(KeyType::A))
-	{
-		SetDir(DIR_LEFT);
-		SetState(DUCK_DOWN_MOVE);
-	}
-	else if (GET_SINGLE(InputManager)->GetButtonDown(KeyType::D))
-	{
-		SetDir(DIR_RIGHT);
-		SetState(DUCK_DOWN_MOVE);
-	}
-	else if (GET_SINGLE(InputManager)->GetButtonUp(KeyType::S))	// 버튼을 뗐을 때
-	{
-		SetState(IDLE);
-	}
+
 }
 
 void Player::TickDuckDownMove()
 {
 	float deltaTime = GET_SINGLE(TimeManager)->GetDeltaTime();
 
-	if (GET_SINGLE(InputManager)->GetButton(KeyType::A))
+	switch (_info.dir())
 	{
-		SetDir(DIR_LEFT);
-		_pos.x -= _playerStat->runSpeed * deltaTime;
-	}
-	else if (GET_SINGLE(InputManager)->GetButton(KeyType::D))
-	{
-		SetDir(DIR_RIGHT);
-		_pos.x += _playerStat->runSpeed * deltaTime;
-	}
-
-	if (GET_SINGLE(InputManager)->GetButtonUp(KeyType::A) || GET_SINGLE(InputManager)->GetButtonUp(KeyType::D))
-	{
-		SetState(DUCK_DOWN);
-	}
-
-	if (GET_SINGLE(InputManager)->GetButtonUp(KeyType::S))
-	{
-		SetState(MOVE);
+	case DIR_LEFT:
+		_pos.x -= _playerStat->crouchSpeed * deltaTime;
+		break;
+	case DIR_RIGHT:
+		_pos.x += _playerStat->crouchSpeed * deltaTime;
+		break;
 	}
 }
 
@@ -373,47 +244,14 @@ void Player::TickJump()
 {
 	float deltaTime = GET_SINGLE(TimeManager)->GetDeltaTime();
 
-	// 좌우 이동도 가능하도록 추가
-	if (GET_SINGLE(InputManager)->GetButton(KeyType::A))
+	switch (_info.dir())
 	{
-		SetDir(DIR_LEFT);
+	case DIR_LEFT:
 		_pos.x -= _playerStat->runSpeed * deltaTime;
-	}
-	else if (GET_SINGLE(InputManager)->GetButton(KeyType::D))
-	{
-		SetDir(DIR_RIGHT);
+		break;
+	case DIR_RIGHT:
 		_pos.x += _playerStat->runSpeed * deltaTime;
-	}
-
-	if (_isGround && !_isAir)	// 땅에 닿았을 때
-	{
-		if (GET_SINGLE(InputManager)->GetButton(KeyType::A))
-		{
-			SetDir(DIR_LEFT);
-			SetState(MOVE);
-		}
-		else if (GET_SINGLE(InputManager)->GetButton(KeyType::D))
-		{
-			SetDir(DIR_RIGHT);
-			SetState(MOVE);
-		}
-		else
-			SetState(IDLE);
-	}
-	else
-	{
-		// ZipLine 
-		if (_nearZipLine)
-		{
-			if (GET_SINGLE(InputManager)->GetButtonDown(KeyType::SpaceBar))
-			{
-				// 바라보는 방향 설정
-				SetDir(DIR_RIGHT);
-
-				_currentZipLine = _nearZipLine;
-				SetState(HANG);
-			}
-		}
+		break;
 	}
  }
 
@@ -435,6 +273,7 @@ void Player::TickCloseAttack()
 		AddComponent(collider);
 	}
 
+	// EndAnimation?
 	if (this->GetIdx() == 6)
 	{
 		SetState(IDLE);
@@ -464,6 +303,7 @@ void Player::TickLongAttack()
 		AddComponent(collider);
 	}
 
+	// 114?
 	if (sumTime <= 0.7f)
 	{
 		if (_info.dir() == DIR_RIGHT)
@@ -474,6 +314,7 @@ void Player::TickLongAttack()
 			_pos.x -= 114 * deltaTime;
 	}
 
+	// EndAnimation?
 	if (this->GetIdx() == 14)
 	{
 		sumTime = 0.0f;
@@ -487,34 +328,6 @@ void Player::TickSkillReady()
 	float deltaTime = GET_SINGLE(TimeManager)->GetDeltaTime();
 	_skillTimer += deltaTime;
 
-	if (GET_SINGLE(InputManager)->GetButtonDown(KeyType::A) && _leftInputCount < 5)
-	{
-		_leftInputCount++;
-	}
-	if (GET_SINGLE(InputManager)->GetButtonDown(KeyType::D) && _rightInputCount < 5)
-	{
-		_rightInputCount++;
-	}
-
-	if (GET_SINGLE(InputManager)->GetButtonDown(KeyType::RightMouse))
-	{
-		if (_leftInputCount > 0 || _rightInputCount > 0)
-		{
-			_skillTimer = 0.f;
-
-			if (_leftInputCount == _rightInputCount)
-				SetState(IDLE);
-			else
-			{
-				if (_leftInputCount < _rightInputCount)
-					SetDir(DIR_RIGHT);
-				else
-					SetDir(DIR_LEFT);
-
-				SetState(SKILL_WAITING);
-			}
-		}
-	}
 	if (_skillTimer >= 2.0f)
 	{
 		_skillTimer = 0.f;
@@ -578,6 +391,7 @@ void Player::TickSkillWaiting()
 
 void Player::TickSkillEnd()
 {
+	// EndAnimation?
 	if (GetIdx() == _flipbookPlayerSkillEnd[_info.dir()]->GetFlipbookEndNum())
 		SetState(IDLE);
 }
@@ -621,26 +435,6 @@ void Player::TickHang()
 		float deltaTime = GET_SINGLE(TimeManager)->GetDeltaTime();
 
 		_pos += direction * speed * deltaTime;
-		//_zipLine->GetPlayerDetectCollider()->SetPos({ _pos });
-
-		// 이동 중 SpaceBar 입력 시 놓기
-		if (GET_SINGLE(InputManager)->GetButtonDown(KeyType::SpaceBar))
-		{
-			isMoving = false;
-
-			sumTime = 0.0f;
-
-			_pos.y -= 50;	// 위치 보정
-
-			_isGround = false;
-			_isAir = true;
-
-			_currentZipLine->SetMidPos(_pos);
-
-			SetState(RELEASE);
-
-			return;
-		}
 
 		// endPos에 도착하면 놓기
 		Vec2 toEnd = endPos - _pos;
