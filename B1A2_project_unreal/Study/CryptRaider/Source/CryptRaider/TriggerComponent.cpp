468e0a2e1c8dea5fa035bb159a45f50495df0920
Im caheeun <dev.chaeng@gmail.com>
2025-11-08 00:45:25 +0900

    (Study)overlapping

---
+++ B1A2_project_unreal/Study/CryptRaider/Source/CryptRaider/TriggerComponent.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TriggerComponent.h"
+
+UTriggerComponent::UTriggerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTriggerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	TArray<AActor*> Actors;
+	GetOverlappingActors(Actors);
+
+	if (Actors.Num() > 0)
+	{
+		FString ActorName = Actors[0]->GetActorNameOrLabel();
+		UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *ActorName);
+	}
+}
