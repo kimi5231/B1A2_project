f0ff775e1196f8f4aeebf47f997d9d2a4a7e8c5b
im-chaeeun <144077145+im-chaeeun@users.noreply.github.com>
2025-11-11 22:46:34 +0900

    .

---
+++ B1A2_project_unreal/TestProject/Intermediate/Build/Win64/UnrealEditor/Inc/TestProject/UHT/SideScrollingAIController.generated.h
@@ -0,0 +1,60 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Variant_SideScrolling/AI/SideScrollingAIController.h"
+
+#ifdef TESTPROJECT_SideScrollingAIController_generated_h
+#error "SideScrollingAIController.generated.h already included, missing '#pragma once' in SideScrollingAIController.h"
+#endif
+#define TESTPROJECT_SideScrollingAIController_generated_h
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+
+// ********** Begin Class ASideScrollingAIController ***********************************************
+TESTPROJECT_API UClass* Z_Construct_UClass_ASideScrollingAIController_NoRegister();
+
+#define FID_GitHub_B1A2_project_B1A2_project_unreal_TestProject_Source_TestProject_Variant_SideScrolling_AI_SideScrollingAIController_h_17_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesASideScrollingAIController(); \
+	friend struct Z_Construct_UClass_ASideScrollingAIController_Statics; \
+	static UClass* GetPrivateStaticClass(); \
+	friend TESTPROJECT_API UClass* Z_Construct_UClass_ASideScrollingAIController_NoRegister(); \
+public: \
+	DECLARE_CLASS2(ASideScrollingAIController, AAIController, COMPILED_IN_FLAGS(CLASS_Abstract | CLASS_Config), CASTCLASS_None, TEXT("/Script/TestProject"), Z_Construct_UClass_ASideScrollingAIController_NoRegister) \
+	DECLARE_SERIALIZER(ASideScrollingAIController)
+
+
+#define FID_GitHub_B1A2_project_B1A2_project_unreal_TestProject_Source_TestProject_Variant_SideScrolling_AI_SideScrollingAIController_h_17_ENHANCED_CONSTRUCTORS \
+	/** Deleted move- and copy-constructors, should never be used */ \
+	ASideScrollingAIController(ASideScrollingAIController&&) = delete; \
+	ASideScrollingAIController(const ASideScrollingAIController&) = delete; \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASideScrollingAIController); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASideScrollingAIController); \
+	DEFINE_ABSTRACT_DEFAULT_CONSTRUCTOR_CALL(ASideScrollingAIController) \
+	NO_API virtual ~ASideScrollingAIController();
+
+
+#define FID_GitHub_B1A2_project_B1A2_project_unreal_TestProject_Source_TestProject_Variant_SideScrolling_AI_SideScrollingAIController_h_14_PROLOG
+#define FID_GitHub_B1A2_project_B1A2_project_unreal_TestProject_Source_TestProject_Variant_SideScrolling_AI_SideScrollingAIController_h_17_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_GitHub_B1A2_project_B1A2_project_unreal_TestProject_Source_TestProject_Variant_SideScrolling_AI_SideScrollingAIController_h_17_INCLASS_NO_PURE_DECLS \
+	FID_GitHub_B1A2_project_B1A2_project_unreal_TestProject_Source_TestProject_Variant_SideScrolling_AI_SideScrollingAIController_h_17_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+class ASideScrollingAIController;
+
+// ********** End Class ASideScrollingAIController *************************************************
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_GitHub_B1A2_project_B1A2_project_unreal_TestProject_Source_TestProject_Variant_SideScrolling_AI_SideScrollingAIController_h
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
