d638c21b2895efb867b0764676242708b41630a3
Im caheeun <dev.chaeng@gmail.com>
2025-11-17 00:41:01 +0900

    .

---
+++ B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/B1A2_UnrealProject.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "B1A2_UnrealProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, B1A2_UnrealProject, "B1A2_UnrealProject" );
+
+DEFINE_LOG_CATEGORY(LogB1A2_UnrealProject)
---
+++ B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/B1A2_UnrealProjectGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "B1A2_UnrealProjectGameMode.h"
+
+AB1A2_UnrealProjectGameMode::AB1A2_UnrealProjectGameMode()
+{
+	// stub
+}
