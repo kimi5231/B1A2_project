8749bae4682cebe5b1a83c5851b1bce97e4867bb
Lim Sooyoung <djwjod@naver.com>
2025-12-26 00:20:18 +0900

    Client IP주소 수정

--- B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/Main.cpp
+++ B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/Main.cpp
@@ -32,7 +32,7 @@ void UMain::Init()
 	SOCKADDR_IN stServerAddr;
 	stServerAddr.sin_family = AF_INET;
 	stServerAddr.sin_port = htons(7777);
-	stServerAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	stServerAddr.sin_addr.s_addr = inet_addr("61.255.49.141");
 
 	// connect
 	nRet = connect(_clientSocket, (sockaddr*)&stServerAddr, sizeof(sockaddr));
@@ -249,7 +249,7 @@ void UMain::RecvAddObject(S_AddObject_Packet addObjectPacket)
 
 		UE_LOG(LogTemp, Log, TEXT("Other Player ID = %d"), id);
 	});
-}
+} 
 
 void UMain::RecvCreateGameRoom(S_CreateGameRoom_Packet packet)
 {
