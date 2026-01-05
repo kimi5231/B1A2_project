34ee6b81d1447d2366dd283d548156de6bbdb219
Lim Sooyoung <djwjod@naver.com>
2026-01-05 10:40:49 +0900

    gitignore 수정 및 파일 정리

--- B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/Main.cpp
+++ B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/Main.cpp
@@ -1,7 +1,5 @@
 ﻿#include "Main.h"
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include "GameNetwork.h"
+#include "Network/GameNetwork.h"
 #include "NetworkRecvRunnable.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
