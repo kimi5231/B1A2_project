316d4211497f4d5e5fe030816a7cf3dad4ecb9b1
Lim Sooyoung <djwjod@naver.com>
2026-01-05 23:07:16 +0900

    GameNetwork 객체를 통해 Recv 처리 구현 및 AddObjectPacket 처리 완료
    +Includes 헤더 추가

--- B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/Network/Includes.h
+++ B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/Network/Includes.h
@@ -1,2 +1,29 @@
 #pragma once
+#define WIN32_LEAN_AND_MEAN
 
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <variant>
+#include <memory> 
+
+#include "Packets.h"
+
+// 매크로 충돌 방지
+#undef min
+#undef max
+
+template <class T>
+struct RecvEvent
+{
+	bool isComplete = false;
+	PacketID packetID;
+	T packetData;
+};
+
+template <class T>
+using RecvEventRef = std::shared_ptr<RecvEvent<T>>;
+
+using RecvEventType = std::variant<RecvEventRef<S_AddObject_Packet>, RecvEventRef<S_Move_Packet>>;
