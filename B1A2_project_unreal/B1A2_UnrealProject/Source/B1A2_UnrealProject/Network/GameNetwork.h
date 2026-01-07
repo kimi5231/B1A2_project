d2c29d13ca063b551eab8efaf0fcb88c717f7959
Lim Sooyoung <djwjod@naver.com>
2026-01-07 18:15:55 +0900

    GameNetwork 클래스에 ProcessSend, CreatePacket 함수 추가

--- B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/Network/GameNetwork.h
+++ B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/Network/GameNetwork.h
@@ -11,6 +11,12 @@ public:
 	void Update();
 	void ProcessRecv();
 
+	template <class T>
+	void ProcessSend(PacketID id, const T& packetData);
+
+	template <class T>
+	std::vector<char> CreatePacket(PacketID id, const T& packetData);
+
 public:
 	std::vector<RecvEventType>& GetRecvEvents() { return _recvEvents; }
 
@@ -22,3 +28,31 @@ private:
 
 	std::vector<RecvEventType> _recvEvents;
 };
+
+template<class T>
+inline void GameNetwork::ProcessSend(PacketID id, const T& packetData)
+{
+	std::vector<char> packet = CreatePacket(id, packetData);
+	int packetSize = packet.size();
+
+	// packetSize 송신(고정 길이)
+	send(_clientSocket, (char*)&packetSize, sizeof(int), 0);
+	// packet 송신(가변 데이터)
+	send(_clientSocket, packet.data(), packetSize, 0);
+}
+
+template<class T>
+inline std::vector<char> GameNetwork::CreatePacket(PacketID id, const T& packetData)
+{
+	// Header
+	Header header;
+	header.id = id;
+	header.dataSize = sizeof(packetData);
+
+	// Packet
+	std::vector<char> packet(sizeof(Header) + header.dataSize);
+	memcpy(packet.data(), &header, sizeof(Header));
+	memcpy(packet.data() + sizeof(Header), &packetData, header.dataSize);
+
+	return packet;
+}
