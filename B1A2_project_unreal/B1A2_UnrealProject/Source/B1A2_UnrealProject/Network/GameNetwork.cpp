265ed0dd1627c7c23ca4b5bac3af6cb854fda653
Lim Sooyoung <djwjod@naver.com>
2026-01-12 01:31:58 +0900

    CreateGameRoom Packet 서버/클라 처리 완료

--- B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/Network/GameNetwork.cpp
+++ B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/Network/GameNetwork.cpp
@@ -136,6 +136,15 @@ void GameNetwork::ProcessRecv()
 		_recvEvents.push_back(event);
 		break;
 	}
+	case S_CreateGameRoom:
+	{
+		NetworkEventRef event = std::make_shared<NetworkEvent>();
+		event->packetID = header.id;
+		event->serializedPacketData.resize(sizeof(S_CreateGameRoom_Packet));
+		memcpy(event->serializedPacketData.data(), packet.data() + sizeof(Header), sizeof(S_CreateGameRoom_Packet));
+		_recvEvents.push_back(event);
+		break;
+	}
 	}
 }
 
