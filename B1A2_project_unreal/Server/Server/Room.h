8ea8e1595ccb68394023162e473ad9a9e3264446
Lim Sooyoung <djwjod@naver.com>
2026-01-16 14:14:17 +0900

    절차적 생성에 필요한 enum class 추가 및 CreateFactoryGameRoom 함수 추가

--- B1A2_project_unreal/Server/Server/Room.h
+++ B1A2_project_unreal/Server/Server/Room.h
@@ -13,23 +13,32 @@ public:
 	void Update();
 
 public:
+	// temp
 	void CreateGameRoom();
+	
+	void CreateFactoryGameRoom();
 
 	GameObjectRef AddObject(ObjectType type);
 
 public:
 	GameObjectRef GetObject(UINT id) { return _players[id]; }
 	const std::unordered_map<UINT, PlayerRef>& GetPlayers() { return _players; }
-
-	// temp
 	const std::vector<GameRoomRef>& GetGameRooms() { return _gameRooms; }
 
 private:
+	// 추후 로그인 기능 추가 후 ID로 대체할 것
 	UINT _generatePlayerID{};
 
 	std::vector<GameRoomRef> _gameRooms;
 	std::unordered_map<UINT, PlayerRef> _players;
 
-	// temp
 	UINT _playerCount{};
+
+private:
+	// 현재 맵의 난이도
+	Difficulty _currentDifficulty{};
+	// 세부 난이도
+	Difficulty _detailDifficulty{};
+
+	int _gameRoomCount{};
 };
