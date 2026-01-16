ff7d875cb8d3e924037b35d271d7e2df7545d43a
Lim Sooyoung <djwjod@naver.com>
2026-01-16 10:20:24 +0900

    Server pch 정리

---
+++ B1A2_project_unreal/Server/Server/Packets.h
@@ -0,0 +1,73 @@
+#pragma once
+#include "Types.h"
+#include <vector>
+
+enum PacketID
+{
+	// Client
+	C_UpdateObjectState,
+	C_Move,
+
+	//Server
+	S_AddObject,
+	S_UpdateObjectState,
+	S_Move,
+	S_CreateGameRoom,
+};
+
+struct Header
+{
+	PacketID id;
+	int dataSize;
+};
+
+// Client
+struct C_UpdateObjectState_Packet
+{
+	int objectID;
+	ObjectType type;
+	MoveState state;
+};
+
+struct C_Move_Packet
+{
+	int objectID;
+	Vector pos;
+	Rotation rotation;
+	MoveState state;
+};
+
+// Server
+struct S_AddObject_Packet
+{
+	int objectID;
+	Vector pos;
+	Rotation rotaion;
+};
+
+struct S_RemoveObject_Packet
+{
+	int objectID;
+	Vector pos;
+	Rotation rotaion;
+};
+
+struct S_UpdateObjectState_Packet
+{
+	int objectID;
+	ObjectType type;
+	MoveState state;
+};
+
+struct S_Move_Packet
+{
+	int objectID;
+	Vector pos;
+	Rotation rotation;
+	MoveState state;
+};
+
+struct S_CreateGameRoom_Packet
+{
+	std::vector<GameRoomInfo> gameRooms;
+};
