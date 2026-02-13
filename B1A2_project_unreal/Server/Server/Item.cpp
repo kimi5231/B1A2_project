1f3b022eca09396356bdbf0df69deafbf80660ab
Lim Sooyoung <djwjod@naver.com>
2026-02-13 19:40:03 +0900

    Item 생성자에 기본 설정 코드 추가

--- B1A2_project_unreal/Server/Server/Item.cpp
+++ B1A2_project_unreal/Server/Server/Item.cpp
@@ -4,6 +4,9 @@
 Item::Item(ItemType itemType)
 	: _itemType(itemType)
 {
+	_pos = { Width / 2 * 10 + 200, Height / 2 * 10, 100 };
+	_rotation = { 0, 0, 0 };
+	_type = ObjectType::Item;
 }
 
 Item::~Item()
