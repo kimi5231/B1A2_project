e3dc704923421ed7a69f6108dd6244fc6b236218
Im caheeun <dev.chaeng@gmail.com>
2026-02-23 00:48:30 +0900

    카메라 Pitch 회전 안 되는 오류 수정

--- B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/MyPlayer.cpp
+++ B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/MyPlayer.cpp
@@ -131,7 +131,7 @@ void AMyPlayer::Look(const FInputActionValue& Value)
 	{
 		// add yaw and pitch input to controller
 		AddControllerYawInput(LookAxisVector.X);
-		AddControllerPitchInput(LookAxisVector.Y);
+		AddControllerPitchInput(-LookAxisVector.Y);
 	}
 }
 
