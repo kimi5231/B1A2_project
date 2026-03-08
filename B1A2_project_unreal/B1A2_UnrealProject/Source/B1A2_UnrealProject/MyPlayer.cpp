0e9d2437b8bf09217e0d5be77b079a3c5e28a98b
Im caheeun <dev.chaeng@gmail.com>
2026-03-09 00:33:20 +0900

    3인칭 -> 1인칭 변경

--- B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/MyPlayer.cpp
+++ B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/MyPlayer.cpp
@@ -23,15 +23,17 @@
 AMyPlayer::AMyPlayer()
 {
 	// Create a camera boom (pulls in towards the player if there is a collision)
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
-	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+	//CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	//CameraBoom->SetupAttachment(RootComponent);
+	//CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
+	//CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+	//FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+	//FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
 	// Create a follow camera
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
-	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+	FollowCamera->SetupAttachment(GetMesh(), FName("head"));
+	FollowCamera->bUsePawnControlRotation = true;
 
 	_isInterpolation = false;
 }
