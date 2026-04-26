5ed1e86545bb4338cebc93bdcdd1efd3686cd6d3
Lim Sooyoung <djwjod@naver.com>
2026-04-26 22:47:48 +0900

    Timer 클래스 임시 추가

---
+++ B1A2_project_unreal/Server/Server/Timer.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "Timer.h"
+
+Timer::Timer()
+{
+    _previousTime = std::chrono::steady_clock::now();
+}
+
+Timer::~Timer()
+{
+}
+
+void Timer::Update()
+{
+    auto currentTime = std::chrono::steady_clock::now();
+    std::chrono::duration<float> frameTime = currentTime - _previousTime;
+    _deltaTime = frameTime.count();
+    _previousTime = currentTime;
+}
