acd6bd7e6566373477fa2a40b82091684051ed21
im-chaeeun <dev.chaeng@gmail.com>
2026-06-22 01:47:46 +0900

    Spider 사이즈 줄임, 부활 시 입력 안 먹었던 문제 IsBusy(몽타주 재생시 입력 차단) 변수 때문인 것 같음
    태스트 필요..

--- B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/Player/MyPlayer.cpp
+++ B1A2_project_unreal/B1A2_UnrealProject/Source/B1A2_UnrealProject/Player/MyPlayer.cpp
@@ -968,7 +968,7 @@ void AMyPlayer::SetCurrentHp(float hp)
 		UAnimInstance* AnimInstance = GetMesh() ? GetMesh()->GetAnimInstance() : nullptr;
 		if (ComboMontage && AnimInstance)
 		{
-			IsBusy = true; // 입력 차단 시작
+			//IsBusy = true; // 입력 차단 시작 <- 아 이거 땜에 입력 안 먹는 것 같음
 
 			// 몽타주 실행
 			float duration = PlayAnimMontage(ComboMontage, 1.f, "Dead");
